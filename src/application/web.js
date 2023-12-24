cd0818f712bd20c1c8da6d19c89fa1fba3ecaad0
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2023-12-25 00:06:37 +0700

    add: dotenv config

--- src/application/web.js
+++ src/application/web.js
@@ -2,6 +2,9 @@ const express = require("express")
 const cors = require('cors')
 const cookieParser = require("cookie-parser")
 const { errorMiddleware } = require("../middleware/error-middleware.js")
+const { config } = require("dotenv")
+
+config()
 
 const web = express()
 
