cb9831bce75538599893f8c1e7edad8d6c7fa6ba
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2023-12-26 12:13:35 +0700

    Redesign DB Schema

--- prisma/migrations/20231225165031_create_all_table/migration.sql
+++ prisma/migrations/20231225165031_create_all_table/migration.sql
@@ -4,37 +4,20 @@ CREATE TABLE `users` (
     `name` VARCHAR(100) NOT NULL,
     `username` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
     `position` VARCHAR(100) NOT NULL,
-    `role` VARCHAR(100) NOT NULL,
-    `isVerified` BOOLEAN NOT NULL,
-    `status` BOOLEAN NOT NULL,
+    `role` VARCHAR(191) NOT NULL DEFAULT 'user',
+    `isVerified` BOOLEAN NOT NULL DEFAULT false,
+    `status` BOOLEAN NOT NULL DEFAULT false,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
+    `verificationToken` VARCHAR(191) NULL,
+    `refreshToken` VARCHAR(191) NULL,
+    `resetPasswordToken` VARCHAR(191) NULL,
 
     UNIQUE INDEX `users_username_key`(`username`),
     UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `refreshTokens` (
-    `userId` INTEGER NOT NULL,
-    `token` VARCHAR(100) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
-
-    PRIMARY KEY (`userId`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `resetPasswordTokens` (
-    `userId` INTEGER NOT NULL,
-    `token` VARCHAR(100) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
-
-    PRIMARY KEY (`userId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
@@ -54,7 +37,7 @@ CREATE TABLE `reports` (
     `category` VARCHAR(100) NOT NULL,
     `subcategory` VARCHAR(100) NOT NULL,
     `description` VARCHAR(255) NOT NULL,
-    `tags` VARCHAR(255) NOT NULL,
+    `tags` TEXT NOT NULL,
     `year` INTEGER NULL,
     `file` VARCHAR(255) NULL,
     `linkFile` VARCHAR(255) NULL,
@@ -100,12 +83,6 @@ CREATE TABLE `contacts` (
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `refreshTokens` ADD CONSTRAINT `refreshTokens_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `resetPasswordTokens` ADD CONSTRAINT `resetPasswordTokens_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `reports` ADD CONSTRAINT `reports_createdUser_fkey` FOREIGN KEY (`createdUser`) REFERENCES `users`(`username`) ON DELETE RESTRICT ON UPDATE CASCADE;
