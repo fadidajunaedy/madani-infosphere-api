1c70791180709533290b7aa545f0121fdf3b4788
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2023-12-27 11:14:31 +0700

    add: report validation

---
+++ src/validation/report-validation.js
@@ -0,0 +1,32 @@
+const Joi = require('joi')
+
+const createReportValidation = Joi.object({
+    title: Joi.string().required(),
+    category: Joi.string.required(),
+    subcategory: Joi.string().required(),
+    description: Joi.string().required(),
+    file: Joi.string().optional(),
+    linkFile: Joi.string.optional(),
+    year: Joi.number().optional(),
+    relatedProgram: Joi.string().optional(),
+    createdUser: Joi.string().required()
+})
+
+const updateReportValidation = Joi.object({
+    title: Joi.string().optional(),
+    category: Joi.string.optional(),
+    subcategory: Joi.string().optional(),
+    description: Joi.string().optional(),
+    file: Joi.string().optional(),
+    linkFile: Joi.string.optional(),
+    year: Joi.number().optional(),
+    relatedProgram: Joi.string().optional()
+})
+
+const getReportValidation = Joi.number().required()
+
+module.exports = {
+    createReportValidation,
+    updateReportValidation,
+    getReportValidation
+}
